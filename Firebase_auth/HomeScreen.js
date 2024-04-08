1e63f9583dbc7b78b329d3abb51eae2818e2a19b
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2024-04-08 22:39:49 +0800

    Home Screen - Cleaning

--- Firebase_auth/HomeScreen.js
+++ Firebase_auth/HomeScreen.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-const HomeScreen = ({ user, handleAuthentication }) => {
-  const logoutUser = async () => {
-    try {
-      await handleAuthentication(null, null);
-      console.log('User logged out successfully!');
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  };
+const HomeScreen = ({}) => {
+
 
   return (
     <View style={{ alignItems: 'center' }}>
-        
-      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Welcome</Text>
-      <Text>{user.email}</Text>
-      <Button title="Logout" onPress={(logoutUser) => handleAuthentication('', '')} color="#e74c3c" />
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Welcome to FinEase</Text>
     </View>
   );
 };
